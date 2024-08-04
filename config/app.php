5677b0b844a8b5354830cf1e45c36c0673bfa9c0
DaviMoreira27 <davimsantana2706@gmail.com>
2024-08-03 22:30:57 -0300

    feat: added correct package

--- config/app.php
+++ config/app.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
+use Monolog\Handler\MongoDBHandler;
 
 return [
 
@@ -159,7 +160,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        
         /*
          * Application Service Providers...
          */
