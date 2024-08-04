a7729b68133e6a881fb81dbf5a6a40c545b279b3
DaviMoreira27 <davimsantana2706@gmail.com>
2024-08-03 21:35:11 -0300

    refactor: adjusted database configuration

--- config/database.php
+++ config/database.php
@@ -35,6 +35,19 @@ return [
 
     'connections' => [
 
+        'mongodb' => [
+            'driver'    => 'mongodb',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', 27017),
+            'database'  => env('DB_DATABASE'),
+            'username'  => env('DB_USERNAME'),
+            'password'  => env('DB_PASSWORD'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
